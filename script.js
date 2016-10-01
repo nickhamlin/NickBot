d4fe8774895f0fdd10f949f846592d172fc34bc3
nickhamlin <nickhamlin@gmail.com>
2016-10-01 15:43:00 -0400

    reverting to example script

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-'use strict';
+/*'use strict';
 
 const _ = require('lodash');
 const Script = require('smooch-bot').Script;
@@ -65,6 +65,44 @@ module.exports = new Script({
             return updateSilent()
                 .then(getSilent)
                 .then(processMessage);
+        }
+    }
+});*/
+
+'use strict';
+
+const Script = require('smooch-bot').Script;
+
+module.exports = new Script({
+    processing: {
+        prompt: (bot) => bot.say('Beep boop...'),
+        receive: () => 'processing'
+    },
+
+    start: {
+        receive: (bot) => {
+            return bot.say('Hi! I\'m Smooch Bot!')
+                .then(() => 'askName');
+        }
+    },
+
+    askName: {
+        prompt: (bot) => bot.say('What\'s your name?'),
+        receive: (bot, message) => {
+            const name = message.text;
+            return bot.setProp('name', name)
+                .then(() => bot.say(`Great! I'll call you ${name}
+Is that OK? %[Yes](postback:yes) %[No](postback:no)`))
+                .then(() => 'finish');
+        }
+    },
+
+    finish: {
+        receive: (bot, message) => {
+            return bot.getProp('name')
+                .then((name) => bot.say(`Sorry ${name}, my creator didn't ` +
+                        'teach me how to do anything else!'))
+                .then(() => 'finish');
         }
     }
 });
